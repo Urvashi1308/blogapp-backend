cd7950710d7855e3a94d6678f5326955425661d4
Your Name <you@example.com>
2021-01-30 13:18:13 +0530

    db connected

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+require('dotenv').config();
 const express=require('express');
+const mongoose = require('mongoose');
 
 //create express app
 const app=express();
+
+
+mongoose.Promise=global.Promise;
+const db = process.env.MONGO_URI;
+mongoose.connect(db,{useUnifiedTopology: true, useNewUrlParser: true }).then(()=>
+    {console.log("DB is connected");
+}    
+).catch((err)=>{
+    console.log(err);
+    process.exit();
+})
 
 const PORT=process.env.PORT||3000;
 app.listen(PORT,(err)=>{
